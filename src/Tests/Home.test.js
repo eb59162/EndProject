a32bcec4e70281e4f8cccfd1b6b02e7f967b037b
eb59162 <stb1641@gmail.com>
2024-06-06 23:48:39 +0300

    controllers and models

--- src/Tests/Home.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from "react-dom"
-import Home from "../OtherComponents/Home";
-
-test('test HOme Componnent',()=>{
-render(<Home/>)
-const linkElement = screen.getByText(/Home/i);
-expect(linkElement).toBeInTheDocument();
-
-})
